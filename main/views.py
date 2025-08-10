3c0c57887d52c3106ede47df3b3dbf57ff6ebc07
Ivona Eftimovska <eftimovskai03@gmail.com>
2025-08-10 14:52:13 +0200

    Adding templates

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from courses.models import Course
+
+
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    courses=Course.objects.all()
+    return render(request, "home.html", context={'courses': courses})
