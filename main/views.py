60cccdbca715a07704bc39ae850fab8a3823099b
Ivona Eftimovska <eftimovskai03@gmail.com>
2025-08-10 16:37:05 +0200

    Courses first version

--- main/views.py
+++ main/views.py
@@ -7,3 +7,7 @@ from courses.models import Course
 def home(request):
     courses=Course.objects.all()
     return render(request, "home.html", context={'courses': courses})
+
+def course(request, course_id):
+    c = Course.objects.get(pk=course_id)
+    return render(request, "course.html", context={'course': c, "courses": Course.objects.all()})
